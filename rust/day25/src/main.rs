4c65e5981299dd44b181fb83ad771745e0f4f34d
rbaron <rbaron@rbaron.net>
2025-08-27 22:49:54 +0200

    Cleanup

--- rust/day25/src/main.rs
+++ rust/day25/src/main.rs
@@ -9,55 +9,6 @@ fn dist(a: &Point, b: &Point) -> i32 {
     zip(a, b).map(|(a1, b1)| (a1 - b1).abs()).sum()
 }
 
-fn get_median(pts: &Vec<Point>, dim: usize) -> Option<&Point> {
-    let mut col = pts.iter().map(|p| p[dim]).collect::<Vec<_>>();
-    col.sort();
-    let mid = col[col.len() / 2];
-    // First point with this median.
-    pts.iter().filter(|p| p[dim] == mid).nth(0)
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct KDTree {
-    point: Point,
-    dim: usize,
-    left: Option<Box<KDTree>>,
-    right: Option<Box<KDTree>>,
-}
-
-fn build_kdtree(pts: &Vec<Point>, dim: usize) -> Option<Box<KDTree>> {
-    if pts.len() == 0 {
-        return None;
-    } else if pts.len() == 1 {
-        return Some(Box::new(KDTree {
-            point: pts[0].clone(),
-            dim: dim,
-            left: None,
-            right: None,
-        }));
-    }
-
-    let mid = get_median(pts, dim).unwrap();
-    let left_pts = pts
-        .iter()
-        .filter(|p| *p != mid && p[dim] < mid[dim])
-        .map(|p| p.clone())
-        .collect::<Vec<_>>();
-
-    let right_pts = pts
-        .iter()
-        .filter(|p| *p != mid && p[dim] >= mid[dim])
-        .map(|p| p.clone())
-        .collect::<Vec<_>>();
-
-    Some(Box::new(KDTree {
-        point: mid.clone(),
-        dim: dim,
-        left: build_kdtree(&left_pts, (dim + 1) % 4),
-        right: build_kdtree(&right_pts, (dim + 1) % 4),
-    }))
-}
-
 fn maybe_merge<'a>(from: &HashSet<&'a Point>, into: &mut HashSet<&'a Point>) -> bool {
     let should_insert = from
         .iter()
@@ -71,7 +22,7 @@ fn maybe_merge<'a>(from: &HashSet<&'a Point>, into: &mut HashSet<&'a Point>) -> 
     should_insert
 }
 
-fn build_clusters(pts: &Vec<Point>, t: Box<KDTree>) {
+fn build_clusters(pts: &Vec<Point>) {
     let mut clusters: VecDeque<HashSet<&Point>> = pts
         .iter()
         .map(|_p: &Vec<i32>| {
@@ -117,25 +68,5 @@ fn main() {
         })
         .collect();
 
-    let t = build_kdtree(&pts, 0);
-    build_clusters(&pts, t.unwrap());
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_median() {
-        let pts = vec![
-            //
-            vec![0, 0, 0, 0],
-            vec![2, 3, 4, 3],
-            vec![1, 1, 2, 10],
-        ];
-        assert_eq!(get_median(&pts, 0), Some(&pts[2]));
-        assert_eq!(get_median(&pts, 1), Some(&pts[2]));
-        assert_eq!(get_median(&pts, 2), Some(&pts[2]));
-        assert_eq!(get_median(&pts, 3), Some(&pts[1]));
-    }
+    build_clusters(&pts);
 }
